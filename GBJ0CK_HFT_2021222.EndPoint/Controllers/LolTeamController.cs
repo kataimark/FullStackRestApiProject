cad3c0621bbad3ad18f5acad44810089803efa1f
kataimark <kataimark@stud.uni-obuda.hu>
2022-11-24 08:42:18 +0100

    EndPoint remake becuse i did an empty according to pdf, but in the video i see its API

---
+++ GBJ0CK_HFT_2021222.EndPoint/Controllers/LolTeamController.cs
@@ -0,0 +1,61 @@
+﻿using GBJ0CK_HFT_2021222.Logic;
+using GBJ0CK_HFT_2021222.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GBJ0CK_HFT_2021222.EndPoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LolTeamController : ControllerBase
+    {
+        ILolTeamLogic logic;
+
+        public LolTeamController(ILolTeamLogic logic)
+        {
+            this.logic = logic;
+        }
+
+
+        // GET: api/<LolPlayerController>
+        [HttpGet]
+        public IEnumerable<LolTeam> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        // GET api/<LolPlayerController>/5
+        [HttpGet("{id}")]
+        public LolTeam Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        // POST api/<LolPlayerController>
+        [HttpPost]
+        public void Create([FromBody] LolTeam value)
+        {
+            this.logic.Create(value);
+        }
+
+        // PUT api/<LolPlayerController>/5
+        [HttpPut("{id}")]
+        public void Update([FromBody] LolTeam value)
+        {
+            this.logic.Update(value);
+        }
+
+        // DELETE api/<LolPlayerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+
+    }
+}
