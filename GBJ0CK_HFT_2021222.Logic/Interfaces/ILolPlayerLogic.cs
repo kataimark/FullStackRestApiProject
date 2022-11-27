3d72dcdaf713df50bb42d5be4cb4b2e056152390
kataimark <kataimark@stud.uni-obuda.hu>
2022-11-27 18:55:29 +0100

    Deleted

--- GBJ0CK_HFT_2021222.Logic/Interfaces/ILolPlayerLogic.cs
+++ GBJ0CK_HFT_2021222.Logic/Interfaces/ILolPlayerLogic.cs
@@ -13,7 +13,6 @@ namespace GBJ0CK_HFT_2021222.Logic
         void Update(LolPlayer item);
 
         IEnumerable<LolPlayer> GetLolplayersAtAgeFourty();
-        IEnumerable<LolPlayer> GetLolplayersAtAgeTwenty();
         IEnumerable<LolPlayer> GetLolPlayerWhereWinIsOverTen();
         IEnumerable<LolPlayer> GetLolplayersWhereTeamNameIsRoll();
     }
