ed18ced4eca0a4faeba52304f8d43b9fb22fc87d
kataimark <kataimark@stud.uni-obuda.hu>
2022-11-27 18:55:20 +0100

    I try to give another one  IEnumerable Lolplayer

--- GBJ0CK_HFT_2021222.Logic/Interfaces/ILolPlayerLogic.cs
+++ GBJ0CK_HFT_2021222.Logic/Interfaces/ILolPlayerLogic.cs
@@ -13,6 +13,7 @@ namespace GBJ0CK_HFT_2021222.Logic
         void Update(LolPlayer item);
 
         IEnumerable<LolPlayer> GetLolplayersAtAgeFourty();
+        IEnumerable<LolPlayer> GetLolplayersAtAgeTwenty();
         IEnumerable<LolPlayer> GetLolPlayerWhereWinIsOverTen();
         IEnumerable<LolPlayer> GetLolplayersWhereTeamNameIsRoll();
     }
